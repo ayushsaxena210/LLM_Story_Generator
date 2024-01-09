16c3788e372cb27350e5afc572fd694cd0ea2048
AYUSH SAXENA <ayushsaxena210@gmail.com>
2024-01-09 19:41:19 +0530

    model & prompt changes

--- generate_story.py
+++ generate_story.py
@@ -9,10 +9,10 @@ openai.api_key = os.getenv("openai_api_key")
 
 def create_story(base_story_prompt):
     response = openai.Completion.create(
-        model="text-davinci-003",
+        model="gpt-3.5-turbo-instruct",
         prompt=base_story_prompt,
-        temperature=0.2,
-        max_tokens=2000,
+        temperature=0,
+        max_tokens=1000,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
@@ -20,12 +20,12 @@ def create_story(base_story_prompt):
     result = response["choices"][0]["text"]
     return result
 
+
 def get_fullstory(base_story, categories):
     prompt = f"""
-    #You have to create a story using 'BaseStory' and 'categories'.
-    #Story should be of length between 100 to 400 charachter. 
+    #You have to create a story using 'BaseStory' and 'categories' and end the story in suspense.
+    #Story should be of length between 200 to 600 charachter. 
     #Begin with a clear concept or theme for your story.
-    #It could be a unique situation, a compelling character, or a thought-provoking question.
     #Most import Story should have good engagement and interesting.
     #Return only Story, No need to give story title or any heading in output.
     BaseStory: ###{base_story}###
@@ -33,6 +33,7 @@ def get_fullstory(base_story, categories):
     """
     full_story = create_story(prompt)
     return full_story
+
 
 def generate_storytitle(story):
     prompt = f"""
@@ -42,22 +43,21 @@ def generate_storytitle(story):
     storytitle = create_story(prompt)
     return storytitle
 
+
 def generate_storytwist(story):
     prompt = f"""
-        Generate 5 twists for the below story.
-        Each twists will be seperated by '^' 
-        Twist should be of length Upto 200 charaters long.
-        Make each twist interesting, and twist can be related to each other.
-         
-        Output Example:
-        twist_1 ^ twist_2 ^ twist_3 ^ twist_4 ^ twist_5
-        
+        # Generate 5 twists for the below story and return output separated by '\n'
+        # Reveal the suspense of the story in twists.
+        # Twist should be of length Upto 200 charaters long.
+        # Make each twist interesting, and some twist can be related to each other.
+
         ######         
         Story: {story}
         ######
         """
     storytwists = create_story(prompt)
     return storytwists
+
 
 def generate_twisttitle(twist):
     prompt = f"""
@@ -67,10 +67,11 @@ def generate_twisttitle(twist):
     twist_title = create_story(prompt)
     return twist_title
 
+
 def generate_story(story, catg):
     storyfull = get_fullstory(story, catg)
     story_title = generate_storytitle(storyfull)
-    story_twists = generate_storytwist(storyfull).split('^')
+    story_twists = generate_storytwist(storyfull).split('\n')[1:]
     story_twists_titles = [generate_twisttitle(twist) for twist in story_twists]
     """
     print('title', story_title)
@@ -79,6 +80,3 @@ def generate_story(story, catg):
     print("Twists titles", story_twists_titles)
     """
     return [story_title, storyfull, story_twists_titles, story_twists]
-
-#story = "there was a boy who live in village, every night he hear some strange sound"
-#catg = ['Horror', 'Trill']
